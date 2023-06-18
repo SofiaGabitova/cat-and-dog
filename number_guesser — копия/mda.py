6897f9f857282098fe0e045429c079eca38c9a5a
SofiaGabitova <sofiya200118@mail.ru>
2023-06-19 02:21:16 +0300

    merger

--- number_guesser — копия/mda.py
+++ number_guesser — копия/mda.py
@@ -3,5 +3,9 @@ if i for range(5):
     print(i)
 print('Животные накормлены, спасибо!')
 
+<<<<<<< Updated upstream
 
 print('Что-то отложила')
+=======
+print('Новая тема: 3.5 Откладывание изменений')
+>>>>>>> Stashed changes
