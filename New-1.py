3a9337d07bc92ccaaf26e403438ad69b8b20f04e
SofiaGabitova <sofiya200118@mail.ru>
2023-06-20 21:00:49 +0300

    group

--- New-1.py
+++ New-1.py
@@ -1,2 +1,15 @@
 print('Новый файл в main и создание КОНФЛИКТА')
 
+import random
+group = ['Магиева', 'Веселовский', 'Кехер']
+bass_players_dict = {'Веселовский' : 142, 'Кехер' : 50, 'Панагов' : 46,
+                         'Данилов' : 3, 'Филь' : 6, 'Коротков' : 8,
+                         'Листочкин' : 12, 'Печенкин' : 22}
+
+
+name = random.choice(group)
+sum_grade_concert = 10 + 5 + 2 + 7 + 9
+date = [5, 6, 7, 4, 6]
+total = round(sum_grade_concert / len(date))
+print(sum_grade_concert / len(date))
+print(total)
