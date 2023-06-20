f71717cdc0714445f64d037dde7008b837e1c8c1
SofiaGabitova <sofiya200118@mail.ru>
2023-06-20 20:58:55 +0300

    code

--- number_guesser — копия/mda.py
+++ number_guesser — копия/mda.py
@@ -1,21 +1,17 @@
-
-
-
-
-
-
-print('Нужно влить ветку!')
-
-
-
-
-
-print('Кто-то ворвался на мою территорию!!!')
-print('Что-то отложила')
-
-
-
-
-
-
-print('+79995678758')
+def conclusion():
+    print('Будут работать:')
+    while True:
+        answer_2 = 2 #int(input('Вывести списком (1) или по пультам (2) ? '))
+        if answer_2 == 1:
+            print(', '.join(team))
+            break
+        elif answer_2 == 2:
+            for i in range(len(team)):
+                print(team[i], end= ' ')
+                if i % 2 == 1:
+                    print()
+                if i == len(team) - 1 and i % 2 == 0:
+                    print()
+            break
+        else:
+            print('Ошибка ввода')
